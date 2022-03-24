b8e8e84e67d76bd7786911de5e2c2ac89f683b7b
zhuangpf <payn_809566095>
2022-03-24 14:41:39 +0800

    :sparkles: Introducing new features.

--- platform-console-service-parent/platform-console-service/src/main/java/com/wordplay/platform/console/ConsoleServiceApplication.java
+++ platform-console-service-parent/platform-console-service/src/main/java/com/wordplay/platform/console/ConsoleServiceApplication.java
@@ -1,14 +1,21 @@
 package com.wordplay.platform.console;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author zhuangpf
  */
+@MapperScan(basePackages = {
+		"com.fallframework.platform.starter.config.mapper",
+		"com.fallframework.platform.starter.i18n.mapper"
+})
 @SpringBootApplication
 @EnableTransactionManagement
+@ComponentScan(basePackages = {"com.fallframework"})
 public class ConsoleServiceApplication {
 
 	public static void main(String[] args) {
