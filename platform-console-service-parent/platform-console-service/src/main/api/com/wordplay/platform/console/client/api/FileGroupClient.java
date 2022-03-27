d72e899bfd487ad2cf2b739ef3d61b63f6787c2f
zhuangpf <payn_809566095>
2022-03-27 17:47:36 +0800

    :sparkles: Introducing new features.

---
+++ platform-console-service-parent/platform-console-service/src/main/api/com/wordplay/platform/console/client/api/FileGroupClient.java
@@ -0,0 +1,31 @@
+package com.wordplay.platform.console.client.api;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fallframework.platform.starter.api.response.ResponseResult;
+import com.fallframework.platform.starter.file.entity.FileGroup;
+import com.fallframework.platform.starter.file.model.FileGroupRequest;
+import com.fallframework.platform.starter.file.model.FileGroupResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 文件组管理
+ *
+ * @author zhuangpf
+ */
+@FeignClient(name = "${platform.console.service.name}${platform.console.service.version:}/${platform.console.service.version}/filegroup")
+public interface FileGroupClient {
+
+	@PostMapping("/delete")
+	ResponseResult delete(@RequestParam Long id);
+
+	@GetMapping("/get")
+	ResponseResult<FileGroupResponse> get(@RequestParam Long id);
+
+	@PostMapping("/list")
+	ResponseResult<Page<FileGroup>> list(@RequestBody FileGroupRequest request);
+
+}
---
+++ platform-console-service-parent/platform-console-service/src/main/java/com/wordplay/platform/console/client/impl/FileGroupClientImpl.java
@@ -0,0 +1,71 @@
+package com.wordplay.platform.console.client.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fallframework.platform.starter.api.response.ResponseResult;
+import com.fallframework.platform.starter.file.entity.FileGroup;
+import com.fallframework.platform.starter.file.entity.FileInfo;
+import com.fallframework.platform.starter.file.model.FileGroupRequest;
+import com.fallframework.platform.starter.file.model.FileGroupResponse;
+import com.fallframework.platform.starter.file.model.FileInfoResponse;
+import com.fallframework.platform.starter.file.service.FileGroupService;
+import com.fallframework.platform.starter.file.service.FileInfoService;
+import com.wordplay.platform.console.client.api.FileGroupClient;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author zhuangpf
+ */
+@Api(tags = "文件组管理")
+@RestController
+@RequestMapping("/filegroup")
+public class FileGroupClientImpl implements FileGroupClient {
+
+	@Autowired
+	private FileGroupService fileGroupService;
+	@Autowired
+	private FileInfoService fileInfoService;
+
+	@Override
+	@PostMapping("/delete")
+	@ApiOperation(value = "删除文件组及明细")
+	public ResponseResult delete(Long id) {
+		fileGroupService.removeById(id);
+		QueryWrapper<FileInfo> wrapper = new QueryWrapper();
+		wrapper.eq("file_group_id", id);
+		fileInfoService.remove(wrapper);
+		return ResponseResult.success();
+	}
+
+	@Override
+	@PostMapping("/get")
+	@ApiOperation(value = "查询文件组及明细")
+	public ResponseResult<FileGroupResponse> get(Long id) {
+		FileGroup fileGroup = fileGroupService.getById(id);
+		QueryWrapper<FileInfo> wrapper = new QueryWrapper();
+		wrapper.eq("file_group_id", id);
+		List<FileInfo> fileInfoList = fileInfoService.list(wrapper);
+		FileGroupResponse fileGroupResponse = new FileGroupResponse();
+		BeanUtils.copyProperties(fileGroup, fileGroupResponse);
+		List<FileInfoResponse> fileInfoResponseList = JSON.parseArray(JSON.toJSONString(fileInfoList), FileInfoResponse.class);
+		fileGroupResponse.setFileInfoResponseList(fileInfoResponseList);
+		return ResponseResult.success(fileGroupResponse);
+	}
+
+	@Override
+	@PostMapping("/list")
+	@ApiOperation(value = "分页查询文件组")
+	public ResponseResult<Page<FileGroup>> list(FileGroupRequest request) {
+		return fileGroupService.list(request);
+	}
+	
+}
