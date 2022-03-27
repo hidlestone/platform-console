3202fff50fa6555d9e345f9803ec58bbc992e704
zhuangpf <payn_809566095>
2022-03-27 18:09:11 +0800

    :sparkles: Introducing new features.

--- platform-console-service-parent/platform-console-service/src/main/java/com/wordplay/platform/console/ConsoleServiceApplication.java
+++ platform-console-service-parent/platform-console-service/src/main/java/com/wordplay/platform/console/ConsoleServiceApplication.java
@@ -3,12 +3,16 @@ package com.wordplay.platform.console;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author zhuangpf
  */
+@EnableFeignClients(
+		basePackages = {"com.wordplay.platform.console.client.api"}
+)
 @MapperScan(basePackages = {
 		"com.fallframework.platform.starter.config.mapper",
 		"com.fallframework.platform.starter.i18n.mapper",
@@ -19,7 +23,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 //		"com.fallframework.platform.starter.task.quartz.mapper",
 //		"com.fallframework.platform.starter.task.xxljob.mapper",
 		"com.fallframework.platform.starter.rbac.mapper",
-		"com.fallframework.platform.test.starters.elasticsearch.mapper"
 })
 @SpringBootApplication
 @EnableTransactionManagement
