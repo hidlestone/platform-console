1ac213e91a9450c058a7ca6bc508ca8b7445c4f3
zhuangpf <payn_809566095>
2022-03-29 18:49:14 +0800

    :sparkles: Introducing new features.

---
+++ platform-console-control-parent/platform-console-control/src/main/java/com/wordplay/platform/console/control/RandomCheckCodeControl.java
@@ -0,0 +1,29 @@
+package com.wordplay.platform.console.control;
+
+import com.fallframework.platform.starter.api.response.ResponseResult;
+import com.wordplay.platform.console.client.api.RandomCheckCodeClient;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhuangpf
+ */
+@Api(tags = "随机校验码")
+@RestController
+@RequestMapping("/verifycode")
+public class RandomCheckCodeControl {
+
+	@Autowired
+	private RandomCheckCodeClient randomCheckCodeClient;
+
+	@PostMapping("/getrandomcheckcode")
+	@ApiOperation(value = "四位随机校验码")
+	public ResponseResult getRandomCheckCode() {
+		return randomCheckCodeClient.getRandomCheckCode();
+	}
+
+}
---
+++ platform-console-control-parent/platform-console-control/src/main/java/com/wordplay/platform/console/control/VerifyCodeControl.java
@@ -0,0 +1,30 @@
+package com.wordplay.platform.console.control;
+
+import com.fallframework.platform.starter.api.response.ResponseResult;
+import com.wordplay.platform.console.client.api.VerifyCodeClient;
+import com.wordplay.platform.console.model.request.VerifyCodeSenderRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhuangpf
+ */
+@Api(tags = "验证码")
+@RestController
+@RequestMapping("/verifycode")
+public class VerifyCodeControl {
+
+	@Autowired
+	private VerifyCodeClient verifyCodeClient;
+
+	@PostMapping("/sendverifycode")
+	@ApiOperation(value = "发送随机的验证码")
+	public ResponseResult sendVerifyCode(VerifyCodeSenderRequest request) {
+		return verifyCodeClient.sendVerifyCode(request);
+	}
+
+}
