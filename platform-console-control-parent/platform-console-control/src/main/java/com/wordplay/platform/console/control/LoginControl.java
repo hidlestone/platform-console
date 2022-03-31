432ca8cf8d75920321c77e0e8243455db6c7cd36
zhuangpf <payn_809566095>
2022-03-31 20:38:20 +0800

    :sparkles: Introducing new features.

--- platform-console-control-parent/platform-console-control/src/main/java/com/wordplay/platform/console/control/LoginControl.java
+++ platform-console-control-parent/platform-console-control/src/main/java/com/wordplay/platform/console/control/LoginControl.java
@@ -2,19 +2,24 @@ package com.wordplay.platform.console.control;
 
 import com.fallframework.platform.starter.api.response.ResponseResult;
 import com.wordplay.platform.console.client.api.LoginClient;
+import com.wordplay.platform.console.model.AccountPwdLoginReq;
 import com.wordplay.platform.console.model.request.AccountPwdLoginRequest;
 import com.wordplay.platform.console.model.request.QRCodeLoginRequest;
 import com.wordplay.platform.console.model.request.VerificationCodeLoginRequest;
 import com.wordplay.platform.console.model.response.LoginSuccessResponse;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author zhuangpf
  */
+@Api(tags = "用户登录")
+@RestController
 @RequestMapping("/${platform.console.control.version}/login")
 public class LoginControl {
 
@@ -23,7 +28,11 @@ public class LoginControl {
 
 	@PostMapping("/loginbyaccount")
 	@ApiOperation(value = "账号密码登录")
-	public ResponseResult<LoginSuccessResponse> loginByAccount(@RequestBody AccountPwdLoginRequest request) {
+	public ResponseResult<LoginSuccessResponse> loginByAccount(@RequestBody AccountPwdLoginReq req) {
+		AccountPwdLoginRequest request = new AccountPwdLoginRequest();
+		request.setAccount(req.getUsername());
+		request.setTel(req.getTel());
+		request.setPassword(req.getPassword());
 		return loginClient.loginByAccount(request);
 	}
 
