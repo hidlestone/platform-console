82145cfdfdae7990f69d82d551e5dfdf2345c4c0
zhuangpf <809566095@qq.com>
2022-05-05 17:53:01 +0800

    feat: 更改角色管理路由

---
+++ doc/hibernate-validator.md
@@ -0,0 +1,12 @@
+# hibernate-validator
+
+
+
+
+
+
+
+
+
+
+
---
+++ platform-console-service-parent/platform-console-service/src/main/resources/db/migration/platform/V1.0_0_202205051725__s_menu_update.sql
@@ -0,0 +1,4 @@
+UPDATE s_menu` SET `id`='3', `parent_id`='1', `level`=NULL, `menu_code`='role_manage', `menu_name`='角色管理', `menu_desc`='角色管理', `path`='/system-manage/role', `func_link`='system-manage/role', `open_type`=NULL, `icon`=NULL, `order`='2', `remark`=NULL, `status`='1', `create_user_id`=NULL, `modify_user_id`=NULL, `gmt_create`='2022-04-02 16:35:36', `gmt_modified`='2022-05-05 17:47:56' WHERE (`id`='3');
+
+
+
