a6492d236d2d034a5d9de7a156d4d6b0b7bc8ecd
zhuangpf <809566095@qq.com>
2022-05-16 14:01:06 +0800

    feat: 优化

--- platform-console-control-parent/platform-console-control/src/main/java/com/wordplay/platform/console/control/SysParamGroupControl.java
+++ platform-console-control-parent/platform-console-control/src/main/java/com/wordplay/platform/console/control/SysParamGroupControl.java
@@ -8,6 +8,7 @@ import com.wordplay.platform.console.model.response.SysParamGroupResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api(tags = "系统参数组")
 @RestController
-@RequestMapping("/${platform.console.control.version}/smstemplate")
+@RequestMapping("/${platform.console.control.version}/sysparamgroup")
 public class SysParamGroupControl {
 
 	@Autowired
 	private SysParamGroupClient sysParamGroupClient;
 
+	@GetMapping("/get")
 	@ApiOperation(value = "根据配置组编码查询配置组及明细项")
 	public ResponseResult<SysParamGroupResponse> get(@RequestParam String code) {
 		return sysParamGroupClient.get(code);
